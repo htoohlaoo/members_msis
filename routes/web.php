73c975f27529ff66aaec86aabe385a8a8c81b815
htoohlaoo <htoohlaoo7@gmail.com>
2025-10-28 07:34:20 +0630

    route permission updated

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::middleware(['auth'])->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::middleware(['auth','role:user'])->group(function () {
+Route::middleware(['auth'])->group(function () {
     Route::post('/member', [MemberController::class, 'subscribe'])->name('members.subscribe');
 });
 
